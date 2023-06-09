ec18bdec96312f41593f4cde21ebce11ceda08ff
Dima <96681581+DimaRT-ED@users.noreply.github.com>
2023-06-09 21:13:59 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 echo " --- Start BUILD AND RUN CONTAINER --- "
 pwd
+echo $GITHUB_JOB >> index.html
 docker run -d --rm --name my-apache-app -p 81:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd
 echo " --- Finish  BUILD AND RUN CONTAINER --- "
