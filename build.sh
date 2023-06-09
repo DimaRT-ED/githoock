58922a473cf4f3c04d4a787a5768e476bedeb6dc
Dima <96681581+DimaRT-ED@users.noreply.github.com>
2023-06-09 21:06:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cat index.html
 echo " --- Start BUILD AND RUN CONTAINER --- "
 pwd
-docker run -d --name my-apache-app -p 81:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd
+docker run -d --rm --name my-apache-app -p 81:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd
 echo " --- Finish  BUILD AND RUN CONTAINER --- "
