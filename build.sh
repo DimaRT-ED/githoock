5eb626792c9919ebab40ce8e29236b2621957584
Dima <96681581+DimaRT-ED@users.noreply.github.com>
2023-06-14 14:48:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo " --- Start BUILD AND RUN CONTAINER --- "
 pwd
 echo "This is build number:${BUILD_ID}   -  " >>  index.html
+echo " Build url is: $BUILD_URL   " >>   index.html
 date >> index.html
 cat index.html
 docker run -d --rm --name my-apache-app -p 81:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd
